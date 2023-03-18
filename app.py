1b3a2d79ed1d4cf638cdc17fc4109f311bddd9a3
ramuthumu <ramu@fastmail.com>
2023-03-18 13:09:03 -0500

    update formatting for output file and extract published date

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import json
 import requests
 import time
 from functools import wraps
 from bs4 import BeautifulSoup
 
 # URL to scrape
-url = "https://www.eenadu.net/telangana/districts/khammam"
+url = "https://www.eenadu.net"
 
 
 def retry_on_exception(max_retries=3, backoff_factor=1):
@@ -32,16 +33,21 @@ def extract_content(url, session):
     soup = BeautifulSoup(response.content, 'html.parser')
     fullstory_div = soup.find('div', {'class': 'fullstory'})
     text = ''
+    heading = ''
+    date_published = ''
     if fullstory_div:
         heading = fullstory_div.find('h1')
         if heading:
-            text += heading.get_text() + '\n\n'
+            heading = heading.get_text()
+        date_published_div = fullstory_div.find('div', {'class': 'eng-body grey pub-t'})
+        if date_published_div:
+            date_published = date_published_div.get_text().strip()
         text_div = fullstory_div.find('div', {'class': 'text-justify'})
         if text_div:
             paragraphs = text_div.find_all('p')
             for p in paragraphs:
                 text += p.get_text() + '\n\n'
-    return text, soup
+    return {'url': url, 'title': heading, 'date_published': date_published, 'content': text}, soup
 
 
 def extract_urls(soup):
@@ -49,12 +55,14 @@ def extract_urls(soup):
     return {a.get('href') for a in anchors if a.get('href') and a.get('href').startswith("https://www.eenadu.net/")}
 
 
-with requests.Session() as session, open('output.txt', 'a') as output_file:
+articles = []
+
+with requests.Session() as session:
     visited = set()
 
     # Extract main content
-    text, main_soup = extract_content(url, session)
-    output_file.write(text)
+    article, main_soup = extract_content(url, session)
+    articles.append(article)
     visited.add(url)
 
     # Extract URLs
@@ -64,8 +72,8 @@ with requests.Session() as session, open('output.txt', 'a') as output_file:
     for url in urls - visited:
         visited.add(url)
         try:
-            text, soup = extract_content(url, session)
-            output_file.write(text)
+            article, soup = extract_content(url, session)
+            articles.append(article)
 
             # Find all the anchor tags on the page and extract the URLs
             link_urls = extract_urls(soup)
@@ -73,9 +81,13 @@ with requests.Session() as session, open('output.txt', 'a') as output_file:
             # Loop through each link URL and extract the text content
             for link_url in link_urls - visited:
                 visited.add(link_url)
-                link_text, _ = extract_content(link_url, session)
-                output_file.write(link_text)
+                link_article, _ = extract_content(link_url, session)
+                articles.append(link_article)
 
         except requests.exceptions.RequestException as e:
             print(f"Error while requesting {url}: {e}")
             continue
+
+# Save the articles to a JSON file
+with open('eenadu.json', 'w') as output_file:
+    json.dump(articles, output_file, ensure_ascii=False, indent=2)
